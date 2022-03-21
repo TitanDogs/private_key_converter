49d5d423e2d3146162f6bc47c0fd738fc5e71c53
Jrej <jrejoire@hotmail.fr>
2022-03-21 23:38:46 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Private Key Converter
+Tool to output your private key from base58 to binary array (64) format.
+This can be used to export a phantom wallet private key into a JSON keyPair for the candy machine setup.
+
+1/ run `npm install` in the root directory.
+
+2/ run `node index.js <YOUR PRIVATE KEY>` to output the new keyPair file.
+
+The file will be saved in the output folder in a JSON format.
+
+# Related video tutorials:
+N/A
---
+++ index.js
@@ -0,0 +1,12 @@
+import { base58_to_binary } from "base58-js";
+import fs from "fs";
+
+let privateKeyString = process.argv[2];
+
+if (privateKeyString) {
+    const binary = base58_to_binary(privateKeyString);
+    fs.writeFileSync(`./output/${privateKeyString}.json`, JSON.stringify(Object.values(binary)));
+    console.log("A JSON keypair was saved in the output folder.")
+} else {
+    console.log("Please add your private key as a string parameter.");
+}

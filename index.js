d8072dedb7f6c1fe367fa66b5e5b817e44a24683
Jrej <jrejoire@hotmail.fr>
2022-06-18 18:35:56 +0200

    fix logs

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import fs from "fs";
 let privateKeyString = process.argv[2];
 
 if (privateKeyString) {
+    if (!fs.existsSync("./output")) {
+        fs.mkdirSync("./output");
+    }
     const binary = base58_to_binary(privateKeyString);
     fs.writeFileSync(`./output/${privateKeyString}.json`, JSON.stringify(Object.values(binary)));
     console.log("A JSON keypair was saved in the output folder.")
